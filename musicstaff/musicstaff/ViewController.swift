db997fb18945261fb0c8abb41809e40b1cb701b1
hzbulesea <hzbulesea@gmail.com>
2018-12-16 14:54:56 -0800

    project create

---
+++ musicstaff/musicstaff/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  musicstaff
+//
+//  Created by Artemis Huang on 12/16/18.
+//  Copyright © 2018 Magic Wand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
